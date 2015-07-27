3eee4bdb6d0a232e0de6e8d7dee5304aa03e95a0
Konstantin Akimov <knstqq@gmail.com>
2015-07-27 03:17:04 +0300

    Added initial simple and working version.

---
+++ reebok.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import http.client, urllib
+import re
+import sys
+import time
+
+def sendMessage(token, user, message):
+    conn = http.client.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+      urllib.parse.urlencode({
+        "token": token,
+        "user": user,
+        "message": message,
+      }), { "Content-type": "application/x-www-form-urlencoded" })
+    response = conn.getresponse()
+
+    print(response.status, response.reason)
+
+    data = response.read()
+    print(len(data))
+    print(data)
+
+
+def getToken(file):
+    tokenFile = open(file)
+    token = tokenFile.read()
+    return token.strip()
+
+def main():
+    user_token = getToken(".user-key")
+    app_token = getToken(".app-key")
+    site_cookie = getToken(".site-cookie")
+    print("tokens: ", user_token, app_token, site_cookie)
+
+    date = "2015-07-27"
+
+    # state:
+    #   -4: start
+    #   -3: error
+    #   -2: empty
+    #   -1: no 50 years
+    #   0+: count in 50 years
+    state = -4
+    while (True):
+        conn = http.client.HTTPConnection("www.reebokinparks.com")
+        conn.request("GET", "/workouts/register/?date=" + date, headers = { "Cookie" : site_cookie });
+        response = conn.getresponse()
+        print("Reebok: ", response.status, response.reason)
+
+        sleepTime = 600
+        newState = -1
+        if response.status == 200:
+            data = str(response.read())
+            print("len of data: ", len(data))
+
+            searchFilled = "<p>Свободных мест на этот день больше нет!</p>"
+            searchEmpty = "<strong>50-летия Октября</strong>,\n.*\n.*осталось мест &mdash; <strong>([0-9]*)</strong>"
+            sFilled = re.search(searchFilled, data)
+            eFilled = re.search(searchEmpty, data)
+            newState = -1
+            if sFilled:
+                newState = -2
+            if eFilled:
+                newState = s.group(1)
+            if newState != state:
+                sleepTime = 60
+            print("state: ", state, " --> ", newState)
+            if state != newState and newState > 0:
+                sendMessage(user_token, app_token, "Check reebok! " + newState + " in 50 years")
+            state = newState
+        else:
+            state = -3
+
+        time.sleep(sleepTime)
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
